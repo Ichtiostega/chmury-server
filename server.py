1e14a2c59f9333a717d90aa593873b8c7e36a188
Ichtiostega <kkna102@gmail.com>
2021-01-08 15:35:35 +0100

    Prep for heroku

--- server.py
+++ server.py
@@ -26,3 +26,6 @@ c = Connector()
 def instruments():
     return c.get_instruments()
 
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
