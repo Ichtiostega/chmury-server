70bba81a8ef1d1bd147e0ca9d57235bb5bd69180
Ichtiostega <kkna102@gmail.com>
2021-01-08 16:15:03 +0100

    Decommented neo

--- app.py
+++ app.py
@@ -2,24 +2,24 @@ from flask import Flask
 from neo4j import GraphDatabase
 app = Flask(__name__)
 
-# class Connector:
-#     def __init__(self, uri, user, password):
-#         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+class Connector:
+    def __init__(self, uri, user, password):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-#     def close(self):
-#         self.driver.close()
+    def close(self):
+        self.driver.close()
 
-#     def get_instruments(self):
-#         with self.driver.session() as session:
-#             instruments = session.write_transaction(self._get_instrument)
-#             return instruments
+    def get_instruments(self):
+        with self.driver.session() as session:
+            instruments = session.write_transaction(self._get_instrument)
+            return instruments
 
-#     @staticmethod
-#     def _get_instrument(tx):
-#         result = tx.run("MATCH (n:Instrument) RETURN n LIMIT 25")
-#         return result.single()[0]
+    @staticmethod
+    def _get_instrument(tx):
+        result = tx.run("MATCH (n:Instrument) RETURN n LIMIT 25")
+        return result.single()[0]
 
-# c = Connector()
+c = Connector('bolt://149.156.109.37:7687', 'u7kocierz', '293170')
 
 
 # @app.route('/instruments', methods=['GET'])
