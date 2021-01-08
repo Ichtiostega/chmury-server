85f096e728fbf92e9e1b59c1ae5b0a52ccf064a9
Ichtiostega <kkna102@gmail.com>
2021-01-08 16:07:34 +0100

    Added neo4j

--- app.py
+++ app.py
@@ -2,29 +2,29 @@ from flask import Flask
 from neo4j import GraphDatabase
 app = Flask(__name__)
 
-class Connector:
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+# class Connector:
+#     def __init__(self, uri, user, password):
+#         self.driver = GraphDatabase.driver(uri, auth=(user, password))
 
-    def close(self):
-        self.driver.close()
+#     def close(self):
+#         self.driver.close()
 
-    def get_instruments(self):
-        with self.driver.session() as session:
-            instruments = session.write_transaction(self._get_instrument)
-            return instruments
+#     def get_instruments(self):
+#         with self.driver.session() as session:
+#             instruments = session.write_transaction(self._get_instrument)
+#             return instruments
 
-    @staticmethod
-    def _get_instrument(tx):
-        result = tx.run("MATCH (n:Instrument) RETURN n LIMIT 25")
-        return result.single()[0]
+#     @staticmethod
+#     def _get_instrument(tx):
+#         result = tx.run("MATCH (n:Instrument) RETURN n LIMIT 25")
+#         return result.single()[0]
 
-c = Connector()
+# c = Connector()
 
 
-@app.route('/instruments', methods=['GET'])
-def instruments():
-    return c.get_instruments()
+# @app.route('/instruments', methods=['GET'])
+# def instruments():
+#     return c.get_instruments()
 
 @app.route('/')
 def index():
