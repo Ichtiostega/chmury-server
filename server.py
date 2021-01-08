3cfe1e9dc2bc7151a20410154ce6bb0491b2f845
Ichtiostega <kkna102@gmail.com>
2021-01-08 15:48:24 +0100

    fix2

--- server.py
+++ server.py
@@ -26,6 +26,9 @@ c = Connector()
 def instruments():
     return c.get_instruments()
 
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
